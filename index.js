648a0cfd65d9fd09bfb847114e1a66c56fa99e1c
Sharmaine Requerme <srqrme@gmail.com>
2018-05-07 18:47:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function increaseRankBy(n) {
   const rank = document.getElementById('app').querySelectorAll('.ranked-list li')
 
   for (let i = 0; i < rank.length; i++) {
-    rank[i].innerHTML = (rank [n].innerHTML) + n.toString()
+    rank[i].innerHTML = (rank[n].innerHTML) + i.toString()
   }
 }

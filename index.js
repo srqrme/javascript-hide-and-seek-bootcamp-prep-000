850ab6346295411034acc327c4e4618bc9bc7ee1
Sharmaine Requerme <srqrme@gmail.com>
2018-05-07 18:44:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ function getFirstSelector(selector) {
 function nestedTarget() {
   return document.querySelector('div#nested div.target')
 }
+
+function increaseRankBy(n) {
+  const rank = document.getElementById('app').querySelectorAll('.ranked-list li')
+
+  for (let i = 0; i < rank.length; i++) {
+    rank[i].innerHTML = (i + 1).toString()
+  }
+}
